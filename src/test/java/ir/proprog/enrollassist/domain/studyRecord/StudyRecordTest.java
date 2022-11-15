36fa4f06e5f12e25e242a703216b0131da7107f8
Daniel-Saeedi <saeedi.danial@gmail.com>
2022-11-15 17:56:07 +0330

    Update StudyRecordTest.java

--- src/test/java/ir/proprog/enrollassist/domain/studyRecord/StudyRecordTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/studyRecord/StudyRecordTest.java
@@ -15,36 +15,134 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StudyRecordTest {
 
-    @DisplayName("Test1")
+    @DisplayName("Students who have passed the course")
     @ParameterizedTest
-    @MethodSource("studyRecordList")
-    public void isPassed(StudyRecord studyRecord) {
-
+    @MethodSource("studyRecordPassedList")
+    public void isPassedShouldReturnTrue(StudyRecord studyRecord) {
+        assertTrue(studyRecord.isPassed(studyRecord.getCourse().getGraduateLevel()));
     }
 
-    private static List<StudyRecord> studyRecordList()
+    @DisplayName("Students who have not passed the course")
+    @ParameterizedTest
+    @MethodSource("studyRecordNotPassList")
+    public void isPassedShouldReturnFalse(StudyRecord studyRecord) {
+        assertFalse(studyRecord.isPassed(studyRecord.getCourse().getGraduateLevel()));
+    }
+
+    private static List<StudyRecord> studyRecordPassedList()
     {
         try {
-            Course software_testing = new Course("01","Software Testing",3,"Undergraduate");
+            Course software_testing = new Course("0000001","Software Testing",3,"Undergraduate");
+            Course neural_networks = new Course("0000002","Neural Networks",3,"Masters");
+            Course advanced_math = new Course("0000003","Advanced Math",4,"PHD");
             return Arrays.asList(
                     new StudyRecord(
-                    "0001",
+                    "40001",
                     software_testing,
                     10),
                     new StudyRecord(
-                            "0002",
+                            "40002",
                             software_testing,
-                            8),
+                            10.1),
                     new StudyRecord(
-                            "0005",
+                            "40003",
                             software_testing,
-                            15)
+                            10.1),
+                    new StudyRecord(
+                            "40001",
+                            software_testing,
+                            15),
+                    new StudyRecord(
+                            "40002",
+                            software_testing,
+                            15),
+                    new StudyRecord(
+                            "40003",
+                            neural_networks,
+                            12),
+                    new StudyRecord(
+                            "40001",
+                            neural_networks,
+                            12.1),
+                    new StudyRecord(
+                            "40002",
+                            neural_networks,
+                            18),
+                    new StudyRecord(
+                            "40002",
+                            advanced_math,
+                            14),
+                    new StudyRecord(
+                            "40003",
+                            advanced_math,
+                            14.1),
+                    new StudyRecord(
+                            "40001",
+                            advanced_math,
+                            18)
             );
 
         } catch (ExceptionList e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private static List<StudyRecord> studyRecordNotPassList()
+    {
+        try {
+            Course software_testing = new Course("0000001","Software Testing",3,"Undergraduate");
+            Course neural_networks = new Course("0000002","Neural Networks",3,"Masters");
+            Course advanced_math = new Course("0000003","Advanced Math",4,"PHD");
+            return Arrays.asList(
+                    new StudyRecord(
+                            "40001",
+                            software_testing,
+                            9.9),
+                    new StudyRecord(
+                            "40002",
+                            software_testing,
+                            1),
+                    new StudyRecord(
+                            "40003",
+                            software_testing,
+                            5),
+                    new StudyRecord(
+                            "40001",
+                            software_testing,
+                            9.9),
+                    new StudyRecord(
+                            "40002",
+                            software_testing,
+                            9),
+                    new StudyRecord(
+                            "40003",
+                            neural_networks,
+                            10),
+                    new StudyRecord(
+                            "40001",
+                            neural_networks,
+                            5),
+                    new StudyRecord(
+                            "40001",
+                            neural_networks,
+                            4),
+                    new StudyRecord(
+                            "40001",
+                            advanced_math,
+                            13.9),
+                    new StudyRecord(
+                            "40001",
+                            advanced_math,
+                            13.5),
+                    new StudyRecord(
+                            "40001",
+                            advanced_math,
+                            10)
+            );
+
+        } catch (ExceptionList e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
